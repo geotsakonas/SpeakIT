db3b22ba675356dbb738d4695edafc2a9a55bc5a
Georgios Tsakonas <tsakwnas.giwrgos@gmail.com>
2022-02-18 15:30:18 +0200

    SpeakIT app

---
+++ src/App.js
@@ -0,0 +1,215 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import { getTokenOrRefresh } from './token_util';
+import './custom.css'
+import { ResultReason } from 'microsoft-cognitiveservices-speech-sdk';
+
+import data from './data.js';
+
+const speechsdk = require('microsoft-cognitiveservices-speech-sdk')
+
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            displayText: 'To start press one the buttons above!',
+            res: []
+        }
+    }
+    
+    async componentDidMount() {
+        // check for valid speech key/region
+        const tokenRes = await getTokenOrRefresh();
+        if (tokenRes.authToken === null) {
+            this.setState({
+                displayText: 'FATAL_ERROR: ' + tokenRes.error
+            });
+        }
+    }
+
+    async sttFromMic() {
+        const tokenObj = await getTokenOrRefresh();
+        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+        speechConfig.speechRecognitionLanguage = 'en-US';
+        
+        const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+
+        this.setState({
+            displayText: 'Speak into your microphone'
+        });
+
+        recognizer.recognizeOnceAsync(result => {
+            let displayText = "";
+            let res = [];
+            const synthesizer = new speechsdk.SpeechSynthesizer(speechConfig);
+       
+            if (result.reason === ResultReason.RecognizedSpeech) {
+                for (const tool of data) {
+                    for (const keyword of tool.keywords) {
+                        if(result.text.includes(keyword)) {
+                            synthesizer.speakTextAsync(
+                                tool.toolName,
+                                result => {
+                                return result.audioData;
+                            },
+                                error => {
+                                console.log(error);
+                                synthesizer.close();
+                            });
+                            
+                            synthesizer.speakTextAsync(
+                                tool.description,
+                                result => {
+                                return result.audioData;
+                            },
+                                error => {
+                                console.log(error);
+                                synthesizer.close();
+                            });
+                            
+                            res.push([tool.toolName, tool.description, tool.link]);
+                            break;
+                        }
+                    }
+                }
+
+                if (res.length === 0) {
+                    synthesizer.speakTextAsync(
+                        "No match was found. All the suggested applications are",
+                        result => {
+                        return result.audioData;
+                    },
+                        error => {
+                        console.log(error);
+                        synthesizer.close();
+                    });
+                    
+                    for (const tool of data) {
+                        res.push([tool.toolName, tool.description]);
+                        synthesizer.speakTextAsync(
+                            tool.toolName,
+                            result => {
+                            return result.audioData;
+                        },
+                            error => {
+                            console.log(error);
+                            synthesizer.close();
+                        });
+                        
+                        synthesizer.speakTextAsync(
+                            tool.description,
+                            result => {
+                            return result.audioData;
+                        },
+                            error => {
+                            console.log(error);
+                            synthesizer.close();
+                        });
+                    }
+                }
+            } else {
+                displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+            }
+
+            synthesizer.speakTextAsync(
+                displayText,
+                result => {
+                synthesizer.close();
+                return result.audioData;
+            },
+                error => {
+                console.log(error);
+                synthesizer.close();
+            });
+
+            this.setState({
+                displayText: displayText,
+                res: res
+            });
+        });
+    }
+
+    async fileChange(event) {
+        const audioFile = event.target.files[0];
+        console.log(audioFile);
+        const fileInfo = audioFile.name + ` size=${audioFile.size} bytes `;
+
+        this.setState({
+            displayText: fileInfo
+        });
+
+        const tokenObj = await getTokenOrRefresh();
+        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
+        speechConfig.speechRecognitionLanguage = 'en-US';
+
+        const audioConfig = speechsdk.AudioConfig.fromWavFileInput(audioFile);
+        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+
+        recognizer.recognizeOnceAsync(result => {
+            let displayText;
+            if (result.reason === ResultReason.RecognizedSpeech) {
+                displayText = `RECOGNIZED: Text=${result.text}`
+    
+
+            } else {
+                displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
+            }
+
+            this.setState({
+                displayText: fileInfo + displayText
+            });
+        });
+    }
+
+    render() {
+        return (
+            <Container className="app-container">
+                <div className="row align-items-md-stretch">
+                    <div className="col-md-6 sp-box">
+                        <div className="h-100 p-4 bg-light border shadow mb-5 box-microphone rounded-lg text-center d-flex align-items-center flex-column justify-content-center">
+                            <h2 style={{color: "#494440"}}>Convert speech to text from your mic</h2>
+                            <div className="mic-btn position-relative">
+                                <i className="fas fa-microphone fa-lg text-center position-absolute" onClick={() => this.sttFromMic()}></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6 sp-box">
+                        <div className="h-100 p-4 bg-light border shadow mb-5 box-file-audio rounded-lg text-center d-flex align-items-center flex-column justify-content-center">
+                            <h2 style={{color: "#494440"}}>Convert speech to text from an audio file</h2>
+                                <label htmlFor="audio-file" className="file-btn position-relative"><i className="fas fa-file-audio fa-lg text-center position-absolute"></i></label>
+                                    <input 
+                                        type="file" 
+                                        id="audio-file" 
+                                        onChange={(e) => this.fileChange(e)} 
+                                        style={{display: "none"}} 
+                                    />
+                        </div>
+                    </div>
+                </div>
+                {(this.state.res.length === 0) ? (
+                <div className="mt-2">
+                    <div className="p-5 bg-light border shadow mb-5 bg-white rounded-lg">
+                        <div>{this.state.displayText}</div>
+                    </div>
+                </div> 
+            ) : (
+                    <div>
+                        {(this.state.res).map((v, index) => 
+                            <div key={index} className="mt-2">
+                                <div className="p-3 bg-light border shadow bg-white rounded-lg">
+                                    <div className="border-bottom color-text-title">{v[0]}</div>
+                                    <div className="color-text-description">{v[1]}</div>
+                                    <div className="bottom-sign"><a className="no-underline" target="_blank" rel="noopener noreferrer" href={v[2]}>Go to tool's page</a></div>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+            )}
+            <div className="bottom-sign p-1"><p>Developed by PROMESIP</p></div>
+            </Container>
+        );
+    }
+}
---
+++ src/data.js
@@ -0,0 +1,62 @@
+export default [
+    {
+		toolName: "Seeing-AI",
+		description: "Seeing-AI is an application that helps the user to recognise objects, people, emotions using the camera.",
+		keywords: ["camera", "Camera", "barcode", "barcodes", "Barcode", "Barcodes", "bar code", "bar codes", "Bar code", "Bar codes", "person", "people", "Person", "People", "emotion", "emotions", "Emotion", "Emotions", "money", "currency", "Money", "Currency", "color", "Color", "Colour", "colour", "colors", "Colors", "Colours", "colours", "handwriting", "handwritten", "Handwriting", "Handwritten", "hand writing", "hand written", "Hand writing", "Hand written"],
+        link: "https://www.microsoft.com/en-us/ai/seeing-ai",
+	},
+	{
+		toolName: "Read and Write",
+		description: "Read&Write is a literacy support tool that offers help with everyday tasks like reading text out loud, understanding unfamiliar words, researching assignments and proofing written work.",
+		keywords: ["revise", "proofread", "Revise", "Proofread", "proofreading", "Proofreading", "fluency", "confidence", "Fluency", "Confidence", "MP3", "IEPs"],
+        link: "https://www.texthelp.com/products/read-and-write-education/",
+	},
+    {
+		toolName: "NaturalReader",
+		description: "Upload text and documents or convert to mp3 to listen to anywhere anytime.",
+		keywords: ["scanned", "document", "documents", "Scanned", "Document", "Documents", "Scan", "scan", "read", "Read", "text", "Text"],
+        link: "https://www.naturalreaders.com/",
+	},
+    {
+		toolName: "Snap&Read",
+		description: "Snap&Read helps with reading text from PDFs, websites and Google Drive. It also supports translation of texts as well as reading from screenshots.",
+		keywords: ["read", "text", "Read", "Text", "PDF", "PDFs", "screenshot", "Screenshots", "translate", "Translate", "translation", "Translation", "note", "notes", "Note", "Notes"],
+        link: "https://snapandread.com/",
+	},
+    {
+		toolName: "Speechify",
+		description: "Power through docs, articles, PDFs, email, anything you read, by listening with our leading text-to-speech reader.",
+		keywords: ["PDF", "PDFs", "email", "emails", "e-mail", "e-mails", "document", "documents", "doc", "docs", "Document", "Documents", "Doc", "Docs"],
+        link: "https://speechify.com/",
+	},
+    {
+		toolName: "Easy Reading",
+		description: "Easy Reading is a software tool supporting cognitive accessibility of web content.",
+		keywords: ["read", "text", "Read", "Text", "Website", "website", "Webpage", "webpage", "access", "Access"],
+        link: "https://www.easyreading.eu/",
+	},
+    {
+		toolName: "Google Translate",
+		description: "Google Translate is a machine translation service developed by Google to translate text, documents and websites from one language into another.",
+		keywords: ["offline", "Offline", "translate", "Translate", "translation", "Translation", "camera", "Camera", "photo", "photos", "Photo", "Photos", "bilingual", "Bilingual", "conversation", "conversations", "Conversation", "Conversations", "handwriting", "handwritten", "Handwriting", "Handwritten", "hand writing", "hand written", "Hand writing", "Hand written"],
+        link: "https://translate.google.gr/",
+	},
+    {
+		toolName: "Be my eyes",
+		description: "Be My Eyes is an application that connects blind and low-vision people with sighted volunteers and company representatives for visual assistance through a live video call.",
+		keywords: ["blind", "Blind", "low", "vision", "visual", "Low", "Vision", "Visual", "assistance", "assistant", "Assistance", "Assistant", "video", "call", "Video", "Call", "volunteer", "Volunteer", "representative", "Representative"],
+        link: "https://www.bemyeyes.com/",
+	},
+    {
+		toolName: "Google Maps",
+		description: "Google Maps is a mapping and navigation application for desktop and mobile devices from Google. It provides turn-by-turn directions to a destination along with 2D and 3D satellite views, as well as public transit information.",
+		keywords: ["Navigation", "navigation", "Maps", "maps", "Airport", "airport", "Directions", "directions"],
+        link: "http://maps.google.gr/",
+	},
+    {
+		toolName: "Uber",
+		description: "Uber is a mobility as a service provider. Its services include ride-hailing, food delivery, package delivery, couriers, freight transportation, electric bicycle and motorized scooter rental via a partnership with Lime, and ferry transport in partnership with local operators.",
+		keywords: ["Taxi", "taxi", "Airport", "airport", "transport", "transportation"],
+        link: "https://www.uber.com/",
+	},
+];
